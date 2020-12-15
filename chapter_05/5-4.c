35528872ff9c6ef17701d3272f7bf6d9e13b4197
Z-bin <1134636325@qq.com>
2020-12-15 23:04:16 +0800

    homework 5-4

---
+++ chapter_05/5-4.c
@@ -0,0 +1,26 @@
+// #define _GNU_SOURCE    // for execvpe()
+#include <stdio.h>
+#include <stdlib.h>  // exit
+#include <unistd.h>  // fork, exec*
+
+int main() {
+  int rc = fork();
+
+  if (rc < 0) {
+    // fork failed; exit
+    fprintf(stderr, "fork failed\n");
+    exit(EXIT_FAILURE);
+  } else if (rc == 0) {
+    char *argvs[] = {"ls", "-l", "-a", "-h", NULL};
+    char *envp[] = {NULL};
+
+    execl("/bin/ls", "ls", "-l", "-a", "-h", NULL);
+    execlp("ls", "ls", "-l", "-a", "-h", NULL);
+    execle("/bin/ls", "ls", "-l", "-a", "-h", NULL, envp);
+
+    execv("/bin/ls", argvs);
+    execvp("ls", argvs);
+    // execvpe("ls", argv, envp);    // GNU extension
+  }
+  return 0;
+}
